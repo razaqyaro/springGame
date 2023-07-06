5c9d14264644c026914d3689b8f4b0f7dc570bba
Abdul-Razak Hussein <abdul-razak.hussein@amalitech.com>
2023-07-06 18:14:05 +0000

    Rename the main class to AppGamingBasicJava

--- src/main/java/com/razzyspring/udemyspringcourse/AppGamingBasicJava.java
+++ src/main/java/com/razzyspring/udemyspringcourse/AppGamingBasicJava.java
@@ -2,4 +2,20 @@ package com.razzyspring.udemyspringcourse;
 
 import com.razzyspring.udemyspringcourse.game.GameRunner;
 import com.razzyspring.udemyspringcourse.game.MarioGame;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class AppGamingBasicJava
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(AppGamingBasicJava.class, args);
+		var marioGame = new MarioGame();
+		var gameRunner = new GameRunner(marioGame);
+		gameRunner.run();
+
+	}
+
+}
