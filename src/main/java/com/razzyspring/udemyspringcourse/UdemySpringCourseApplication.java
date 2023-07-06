c01016ae900d49beb726ed6096b746bc3520c9b2
Abdul-Razak Hussein <abdul-razak.hussein@amalitech.com>
2023-07-06 17:52:03 +0000

    Initial commit

---
+++ src/main/java/com/razzyspring/udemyspringcourse/UdemySpringCourseApplication.java
@@ -0,0 +1,13 @@
+package com.razzyspring.udemyspringcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/razzyspring/udemyspringcourse/UdemySpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razzyspring.udemyspringcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
